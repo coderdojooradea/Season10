9f3f9b5b56261877eefd4aaf813eea7d82345f25
Daniel Erzse <derzse@gmail.com>
2023-09-24 09:47:49 +0300

    Initial project

--- Python/S01/project.py
+++ Python/S01/project.py
@@ -1,1 +1,47 @@
+# Capstone Project: Personal Expense Tracker
+
+# TODO: Create a list to store expenses. Each expense will be a dictionary.
+
+expenses = []
+
+# TODO: Define a function to add an expense.
+def add_expense():
+    # Get input for amount, category, and description.
+    # Ensure data validation where necessary.
+    pass
+
+# TODO: Define a function to view all expenses.
+def view_all_expenses():
+    pass
+
+# TODO: Define a function to search expenses by category.
+def search_by_category():
+    pass
+
+# TODO: Define a function to calculate the total expenditure.
+def total_expenditure():
+    pass
+
+# TODO: Define a function to delete an expense.
+def delete_expense():
+    pass
+
+# TODO: Define a function to display the user menu and handle user choices.
+def main_menu():
+    while True:
+        # Display menu options to the user.
+        # Use conditional statements to call the appropriate function based on user choice.
+        pass
+
+# TODO (Optional): Define a function to set a monthly budget.
+def set_budget():
+    pass
+
+# TODO (Optional): Define a function to analyze expenses.
+def expense_analysis():
+    pass
+
+# Entry point of the application.
+if __name__ == "__main__":
+    main_menu()
 
