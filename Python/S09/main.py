5cdf08b621825fbefb9990c4f916de5b87ee717e
Daniel Erzse <derzse@gmail.com>
2024-03-03 10:43:00 +0200

    Update main.py

--- Python/S09/main.py
+++ Python/S09/main.py
@@ -7,6 +7,10 @@ try:
     ser = serial.Serial(port, baudrate=baud_rate)
     print(f"Conectat la portul {port} cu rata de {baud_rate} baud")
 
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(f"Date primite: {line}")
 
 except serial.SerialException as e:
     print(f"Eroare la conectarea la portul {port}: {e}")
