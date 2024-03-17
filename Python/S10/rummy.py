8963d46129799e75e8b46709d04afe41b89e58b8
Daniel Erzse <derzse@gmail.com>
2024-03-17 10:59:34 +0200

    Update rummy.py

--- Python/S10/rummy.py
+++ Python/S10/rummy.py
@@ -19,4 +19,37 @@ RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 SUITS = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
 
 deck = [(rank,suit) for rank in RANKS for suit in SUITS]
-print(deck)
+
+
+# Functions
+# Shuffle the deck
+def shuffle_deck():
+    random.shuffle(deck)
+
+# Draw card
+def draw_card():
+    if deck:
+        return deck.pop()
+    else:
+        return None
+
+# Deal cards
+def deal_cards(num_players):
+    hands = [[] for _ in range(num_players)]  
+    for _ in range(10):
+        for i in range(num_players):
+            hands[i].append(draw_card())
+    return hands
+
+# Main function
+def main()
+    # Shuffle the deck
+    shuffle_deck()
+
+    # Deal cards
+    hands = deal_cards(2)
+    player_hand = hands[0]
+    opponent_hand = hands[1]
+
+    # First player is human
+    curent_player = 0 
