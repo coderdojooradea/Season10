1ec493213261e3574882a26db963b77f31851f8b
Daniel Erzse <derzse@gmail.com>
2024-04-14 22:44:13 +0300

    Update weather.py

--- Python/S13/weather.py
+++ Python/S13/weather.py
@@ -2,74 +2,75 @@ import requests
 import streamlit as st
 import pydeck as pdk
 
-
+# URLs and API key setup
 geo_url = "http://api.openweathermap.org/geo/1.0/direct?"
 weather_url = "https://api.openweathermap.org/data/2.5/weather?"
 api_key = "6c64d4a25dfad806e5ef1cddae5a5acf"
 
 def get_lat_lon(city, key):
-    geo_url_ll = geo_url +f"q={city}&limit=3&appid={key}"
-    print(geo_url_ll)
+    """Fetch latitude and longitude for a given city."""
+    geo_url_ll = f"{geo_url}q={city}&limit=3&appid={key}"
     response = requests.get(geo_url_ll)
-    lat = response.json()[0]['lat']
-    lon = response.json()[0]['lon']
-    return city, lat, lon
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            lat = data[0]['lat']
+            lon = data[0]['lon']
+            return city, lat, lon
+    return None, None, None
 
 def get_weather_ll(lat, lon, key):
-    ll_url = weather_url + f"lat={lat}&lon={lon}&appid={key}"
+    """Fetch weather data using latitude and longitude."""
+    ll_url = f"{weather_url}lat={lat}&lon={lon}&appid={key}"
     response = requests.get(ll_url)
-    print(response.json())
-    weather = response.json()['weather'][0]
-    description = weather['main']
-    icon = weather['icon']
-    main= response.json()['main']
-    temp = main['temp']
-    pres = main['pressure']
-    hum = main['humidity']
-    wind = response.json()['wind']['speed']
-    resolution = {'description': description,
-                  'icon': icon, 
-                  'temp': temp, 
-                  'pressure':pres, 
-                  'humidity': hum, 
-                  'wind': wind}
-    return resolution 
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['weather'][0]
+        description = weather['main']
+        icon = weather['icon']
+        main = data['main']
+        temp = main['temp']
+        pres = main['pressure']
+        hum = main['humidity']
+        wind = data['wind']['speed']
+        resolution = {'description': description,
+                      'icon': icon, 
+                      'temp': temp, 
+                      'pressure': pres, 
+                      'humidity': hum, 
+                      'wind': wind}
+        return resolution
+    return None
 
 
 def display_map(lat, lon):
+    """Display a map centered at the specified latitude and longitude."""
     view_state = pdk.ViewState(
-        latitude = lat,
-        longitude = lon,
-        zoom = 11, pitch = 0
+        latitude=lat,
+        longitude=lon,
+        zoom=11,
+        pitch=0
     )
-
     layer = pdk.Layer(
         'ScatterplotLayer',
-        data = [{'position': [lon, lat], 
-               'color': [255,0,0], 
-               'radius': 100}],
-        get_position ='position',
-        get_color = 'color',
-        get_radius = 'radius'       
+        data=[{'position': [lon, lat], 'color': [255, 0, 0], 'radius': 100}],
+        get_position='position',
+        get_color='color',
+        get_radius='radius'       
     )
-
-    map=pdk.Deck(
-        layers = [layer],
-        initial_view_state = view_state,
-        map_style = 'mapbox://styles/mapbox/light-v9'
+    map = pdk.Deck(
+        layers=[layer],
+        initial_view_state=view_state,
+        map_style='mapbox://styles/mapbox/light-v9'
     )
-
     st.pydeck_chart(map)
 
+
 def display_weather(data):
-
-    # Construct the URL for the weather icon
+    """Display weather data along with the weather icon."""
+    st.title("Weather Data")
     icon_url = f"http://openweathermap.org/img/wn/{data['icon']}.png"
-
-    # Convert temperature from Kelvin to Celsius for display
-    temp_celsius = data['temp'] - 273.15
-
-    # Using columns to display text and image side by side
+    temp_celsius = data['temp'] - 273.15  # Convert temperature from Kelvin to Celsius
     col1, col2 = st.columns([3, 1])
     with col1:
         st.write(f"**Description**: {data['description']}")
@@ -79,7 +80,6 @@ def display_weather(data):
         st.write(f"**Wind Speed**: {data['wind']} m/s")
     with col2:
         st.image(icon_url, caption="Current Weather")
-
 def main():
     """Display weather data along with the weather icon."""
     st.title("Weather Data")
